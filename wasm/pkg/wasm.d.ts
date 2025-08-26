ba9da82aa9936b25f098bcdd94abc2f2999d0591
Vašek Potoček <vasek.potocek@post.cz>
2025-08-26 16:29:16 +0200

    Include wasm binary for Pages

---
+++ wasm/pkg/wasm.d.ts
@@ -0,0 +1,37 @@
+/* tslint:disable */
+/* eslint-disable */
+export function process(left: Float32Array, right: Float32Array, handle: number): void;
+export function get_sample(left: Float32Array, right: Float32Array, handle: number): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly instance_new_handle: (a: number) => number;
+  readonly process: (a: number, b: number, c: any, d: number, e: number, f: any, g: number) => void;
+  readonly get_sample: (a: number, b: number, c: any, d: number, e: number, f: any, g: number) => void;
+  readonly __wbindgen_export_0: WebAssembly.Table;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ wasm/pkg/wasm_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const instance_new_handle: (a: number) => number;
+export const process: (a: number, b: number, c: any, d: number, e: number, f: any, g: number) => void;
+export const get_sample: (a: number, b: number, c: any, d: number, e: number, f: any, g: number) => void;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_start: () => void;
